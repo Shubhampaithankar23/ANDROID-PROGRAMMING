23327d1daa025821f5139b2ce10a5115dc101b6c
Shubham <sandippaithankar73@gmail.com>
2025-02-07 14:30:56 +0530

    Update and rename README.md to Join two page.kt

---
+++ Join two page.kt
@@ -0,0 +1,7 @@
+val MainAct2button = findViewById<Button>(R.id.main_act2_button)
+
+MainAct2button.setOnClickListener {
+
+val Intent Intent(packageContext: this, MainActivity2::class.java)
+
+startActivity(Intent)
