ab5b50fd37682eaf28c957ed19fdee64dfd93f1b
Shubham <sandippaithankar73@gmail.com>
2025-07-31 18:24:25 +0530

    Delete Join two page.kt

--- Join two page.kt
+++
@@ -1,7 +0,0 @@
-val MainAct2button = findViewById<Button>(R.id.main_act2_button)
-
-MainAct2button.setOnClickListener {
-
-val Intent Intent(packageContext: this, MainActivity2::class.java)
-
-startActivity(Intent)
